ba515a040a828b8e4c2f2e43f762f7a3e05f6bb5
David Dever <david.dever@cox.net>
2016-01-24 13:39:48 -0800

    testing api

--- addon.py
+++ addon.py
@@ -2,52 +2,23 @@
 import xbmcaddon
 import xbmcgui
 import os
-
-import socket
 import sys
-import binascii
 import colorsys
-import struct
 
-from binascii import unhexlify
+from lifxlan import *
 
-def long_to_bytes (val):
-   
-    val = long(val);  
-    val = struct.pack('<Q',val);
-    val = val[::-1]
-    val = ''.join( [ "%02X" % ord( x ) for x in val ] ).strip();
-    return val[12:16]
+#def get_bulb_ips:
+#        ips = []
+#        num_lights = None
+#        lifx = lifxlan.LifxLAN(num_lights)
+#        devices = lifxlan.lifx.get_lights()     
 
-def get_bulb_ip (): 
+#	for d in devices:
+#        	ips.append(d.ip_addr)
 
-	sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-	sender.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	sender.connect(('255.255.255.255', 56700));
+#	return ips
 
-	listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-	listener.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-	listener.bind(('',56700));
-
-	ownIp = socket.gethostbyname(socket.gethostname());
-
-	packetArray = bytearray([0x24, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00])
-
-	sender.send(packetArray);
-
-	sender.close();
-
-	deviceIp = ownIp;
-
-	while deviceIp == ownIp:
-		data, addr =listener.recvfrom(2048);
-		deviceIp = addr[0];
-
-	listener.close();
-	sender.close();
-
-	return addr[0];
-
+lifxlan=LifxLAN()
 
 useLegacyApi   = True
 capture = xbmc.RenderCapture()
@@ -55,12 +26,13 @@ capture = xbmc.RenderCapture()
 if useLegacyApi:
 	capture.capture(32, 32, xbmc.CAPTURE_FLAG_CONTINUOUS)
 
-host = get_bulb_ip()
-port = 56700
+color = []
+#lights = get_bulb_ips()
 
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-s.connect((host, port))
+#print 'Found LIFX lights'
+#for d in lights:
+#	print d
+ 
 
 class PlayerMonitor( xbmc.Player ):
 	def __init__( self, *args, **kwargs ):
@@ -69,7 +41,6 @@ class PlayerMonitor( xbmc.Player ):
 	def onPlayBackStarted( self ):
 		if not useLegacyApi:
 			capture.capture(32, 32)
-
 
 
 while not xbmc.abortRequested:
@@ -103,29 +74,19 @@ while not xbmc.abortRequested:
 		hsb = colorsys.rgb_to_hsv(red, green, blue);
 
 		huevalue = int(hsb[0]*65535);
-		huevalueHex = long_to_bytes(huevalue);
 		satvalue = int(hsb[1]*65535);
-		satvalueHex = long_to_bytes(satvalue);
 		brightnessvalue = int(hsb[2]*65535);
-		brightnessvalueHex = long_to_bytes(brightnessvalue);
 
-		packetArray = bytearray([0x31,0x00,0x00,0x34,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x66,0x00,0x00,0x00,0x00,0x55,0x55,0xFF,0xFF,0xFF,0xFF,0xAC,0x0D,0x00,0x00,0x00,0x00])
-
-		packetArray[38] = unhexlify(huevalueHex[0:2]);
-		packetArray[37] = unhexlify(huevalueHex[2:4]);
-
-		packetArray[40] = unhexlify(satvalueHex[0:2]);
-		packetArray[39] = unhexlify(satvalueHex[2:4]);
-
-		packetArray[43] = unhexlify(brightnessvalueHex[0:2]);
-		packetArray[42] = unhexlify(brightnessvalueHex[2:4]);
+                color.append(huevalue)
+                color.append(satvalue)
+                color.append(brightnessvalue)
+                color.append(4000)
 
 		try:
-			s.send(packetArray);
+			lifxlan.set_color_all_lights(color, rapid=True)
 		except:
 			print "Caught exception socket.error"
 
-s.close()
 
 if ( __name__ == "__main__" ):
 
